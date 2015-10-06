e806fbe2fbffe1bd683c25c3b6bb77066859fbbd
Marco <polo177@gmail.com>
2015-10-06 16:01:56 -0500

    tidy. added commented lines for stdin warning

--- provision.pl
+++ provision.pl
@@ -10,18 +10,20 @@ my $user;
 my $transfer = 1;
 help() if ( @ARGV < 1 or 5 < @ARGV );
 GetOptions(
-    "system=s" => \$system,
-    "user=s"   => \$user,
-    "transfer!"   => \$transfer,
-    "help"     => \$help
+    "system=s"  => \$system,
+    "user=s"    => \$user,
+    "transfer!" => \$transfer,
+    "help"      => \$help
 ) or die("Error in command line arguments\n");
 help() if ( defined $help );
-$system = $ARGV[0] if ( ! defined $system );
+$system = $ARGV[0] if ( !defined $system );
 my $sys_address_for_scp = $system;
-if ( ! defined $user || $user eq '' ) {
+
+if ( !defined $user || $user eq '' ) {
     if ( defined $ARGV[1] && $ARGV[1] !~ /\./ ) {
         $user = $ARGV[1];
-    }else{
+    }
+    else {
         $user = 'root';
     }
 }
@@ -31,16 +33,16 @@ make_tmp_dir();
 
 set_sysip_prompt() if ( $sys_address_for_scp =~ /(\d{1,3}\.){3}\d{1,3}/ );
 
-unless ( -e "system.plans/${user}\@$system" ){
+unless ( -e "system.plans/${user}\@$system" ) {
     $system = 'CPANEL';
 }
-chomp( my @files = read_file( "system.plans/${user}\@$system" ) );
+chomp( my @files = read_file("system.plans/${user}\@$system") );
 my @use_ssh_key;
 my @use_port;
 foreach my $file ( sort @files ) {
     $file =~ s/~/$ENV{HOME}/g;
     if ( $file =~ /\.ssh/ ) {
-        system( 'cp', "${file}.pub", "$dir_for_files/ssh_key");
+        system( 'cp', "${file}.pub", "$dir_for_files/ssh_key" );
         if ( $file =~ /(.*)\.pub$/ ) {
             $file = $1;
         }
@@ -53,34 +55,38 @@ foreach my $file ( sort @files ) {
         my $file_part;
         if ( $file =~ /(.*):SNR:(.*):(.*)/ ) {
             my ( $filename, $search, $replace ) = ( $1, $2, $3 );
-            $file_part = read_file( "files/$filename" );
+            $file_part = read_file("files/$filename");
             $file_part =~ s/$search/$replace/g;
-        } else {
-            $file_part = read_file( "files/$file" );
-        };
-        write_file( "$dir_for_files/.bash_custom", {append => 1 }, "\n# $file\n" . $file_part );
+        }
+        else {
+            $file_part = read_file("files/$file");
+        }
+        write_file( "$dir_for_files/.bash_custom", { append => 1 }, "\n# $file\n" . $file_part );
     }
-    elsif ( $file =~ /(.*):SNR:(.*):(.*)/ ) { # so can't use colons in the regex
+    elsif ( $file =~ /(.*):SNR:(.*):(.*)/ ) {    # so can't use colons in the regex
         my ( $filename, $search, $replace ) = ( $1, $2, $3 );
-        system( "cp files/$filename $dir_for_files/" );
+        system("cp files/$filename $dir_for_files/");
         replace_text_in_file( $dir_for_files, $filename, $search, $replace );
     }
-    else { # default files going to user's home dir on destination
-        system( "cp files/$file $dir_for_files/" );
+    else {                                       # default files going to user's home dir on destination
+        system("cp files/$file $dir_for_files/");
     }
 }
 
 system( 'tar', '-cvf', 'totransfer.tar', $dir_for_files );
-if ( $transfer ) {
+if ($transfer) {
     my $place = "${user}\@$sys_address_for_scp";
-    system( 'scp', '-q', @use_ssh_key, @use_port, 'totransfer.tar', "$place:transferred_by_provision_script.tar" );
-    system( 'scp', '-q', @use_ssh_key, @use_port, 'expand.pl', "$place:provision_expand.pl" );
-}
 
+    # for 'stdin: is not a tty' error
+    # `scp -q @use_ssh_key @use_port totransfer.tar $place:transferred_by_provision_script.tar >/dev/null 2>&1`;
+    # `scp -q @use_ssh_key @use_port expand.pl $place:provision_expand.pl >/dev/null 2>&1`;
+    system( 'scp', '-q', @use_ssh_key, @use_port, 'totransfer.tar', "$place:transferred_by_provision_script.tar" );
+    system( 'scp', '-q', @use_ssh_key, @use_port, 'expand.pl',      "$place:provision_expand.pl" );
+}
 
 # subroutines
 sub make_tmp_dir {
-    if ( -d $dir_for_files ) { # transfer will keep this tmp files dir
+    if ( -d $dir_for_files ) {    # transfer will keep this tmp files dir
         system( 'rm', '-rvf', "${dir_for_files}.bak" );
         system( 'mv', '-v', $dir_for_files, "${dir_for_files}.bak" );
     }
@@ -88,6 +94,7 @@ sub make_tmp_dir {
 }
 
 sub set_sysip_prompt {
+
     # TODO: use CPANEL by default if system is an IP address without a system file
     open( my $fh, '>>', "$dir_for_files/.bash_custom" ) or die "Couldn't open file $!";
     print $fh "hostip=$sys_address_for_scp\n";
@@ -96,10 +103,10 @@ sub set_sysip_prompt {
 sub replace_text_in_file {
     my ( $dir, $filename, $search, $replace ) = @_;
     use Path::Tiny qw(path);
-    my $file = path( "$dir/$filename" );
+    my $file = path("$dir/$filename");
     my $data = $file->slurp_utf8;
     $data =~ s/$search/$replace/g;
-    $file->spew_utf8( $data );
+    $file->spew_utf8($data);
 }
 
 sub help {
@@ -111,5 +118,5 @@ sub help {
 }
 
 # cleanup
-system( 'rm', '-rf', $dir_for_files ) if ( $transfer );
-system( 'rm', '-rf', 'totransfer.tar' ) if ( $transfer );
+#system( 'rm', '-rf', $dir_for_files )   if ($transfer);
+system( 'rm', '-rf', 'totransfer.tar' ) if ($transfer);
